fb5864fd68aa00d462ce73228ffaa82288066679
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2022-05-06 15:11:53 +0200

    test: provide test for effects

---
+++ src/app/book/store/book-collection.effects.spec.ts
@@ -0,0 +1,39 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { createServiceFactory } from '@ngneat/spectator';
+import { EffectsModule } from '@ngrx/effects';
+import { Store, StoreModule } from '@ngrx/store';
+import { of } from 'rxjs';
+import { BookApiService } from '../book-api.service';
+import { bookNa } from '../models';
+import { booksLoadingActions } from './book-collection.actions';
+import { BookCollectionEffects } from './book-collection.effects';
+import { bookCollection } from './book-collection.selectors';
+import { bookFeatureName, bookFeatureReducers } from './book.feature';
+
+describe(BookCollectionEffects.name, () => {
+  const createService = createServiceFactory({
+    service: Store,
+    imports: [
+      StoreModule.forRoot({}, {}),
+      StoreModule.forFeature(bookFeatureName, bookFeatureReducers),
+      EffectsModule.forRoot([BookCollectionEffects]),
+      RouterTestingModule
+    ],
+    providers: [{ provide: BookApiService, useFactory: () => ({ getAll: () => of([bookNa()]) }) }]
+  });
+
+  describe('When a book is created successfully', () => {
+    it('is accessible in the store', done => {
+      const service = createService();
+
+      const store = service.inject(Store);
+
+      store.dispatch(booksLoadingActions.loadingStarted());
+
+      store.select(bookCollection).subscribe((books: any) => {
+        expect(books.length).toBe(1);
+        done();
+      });
+    });
+  });
+});
