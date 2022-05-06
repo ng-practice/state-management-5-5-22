04041474ae2d7788672c2ea459ab54f279393cf3
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2022-05-06 14:23:15 +0200

    feat: introduce spectator

--- src/app/book/book-list/book-list.component.spec.ts
+++ src/app/book/book-list/book-list.component.spec.ts
@@ -1,5 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { byTestId, createComponentFactory } from '@ngneat/spectator';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { bookNa } from '../models';
@@ -7,29 +6,21 @@ import { bookCollection } from '../store';
 import { BookListComponent } from './book-list.component';
 
 describe(BookListComponent.name, () => {
-  let fixture: ComponentFixture<BookListComponent>;
-  let store: MockStore;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [BookListComponent, BookCardComponent],
-      providers: [provideMockStore()]
-    });
-
-    store = TestBed.inject(MockStore);
+  const createComponent = createComponentFactory({
+    component: BookListComponent,
+    declarations: [BookCardComponent],
+    providers: [provideMockStore()]
   });
 
   describe('When books are present', () => {
     it('renders books', () => {
+      const spectator = createComponent();
+
+      const store = spectator.inject(MockStore);
       store.overrideSelector(bookCollection as any, [bookNa(), bookNa()]);
 
-      fixture = TestBed.createComponent(BookListComponent);
-      fixture.detectChanges();
-
-      // import { By } from '@angular/platform-browser';
-      const bookCards = fixture.debugElement.queryAll(By.css('[data-test=book-card]'));
-
-      expect(bookCards.length).toBe(2);
+      const bookCards = spectator.queryAll(byTestId('book-card'));
+      expect(bookCards).toHaveLength(3);
     });
   });
 });
